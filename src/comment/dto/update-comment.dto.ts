8a5f1f54fdc60c06e809a0a08dd1d8e1c42b9134
mnxmnz <minzidev@gmail.com>
2025-03-15 18:23:30 +0900

    feat: UpdateCommentDto 및 UpdateProductDto Swagger API 속성 추가

--- src/comment/dto/update-comment.dto.ts
+++ src/comment/dto/update-comment.dto.ts
@@ -1,4 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { PartialType } from '@nestjs/swagger';
 import { CreateCommentDto } from './create-comment.dto';
 
-export class UpdateCommentDto extends PartialType(CreateCommentDto) {}
+export class UpdateCommentDto extends PartialType(CreateCommentDto) {
+  @ApiProperty({
+    description: 'The content of the comment',
+    example: 'This is a comment',
+  })
+  public content: string;
+}
--- src/product/dto/update-product.dto.ts
+++ src/product/dto/update-product.dto.ts
@@ -1,4 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+  @ApiProperty({
+    description: 'The name of the product',
+    example: 'iPhone 15',
+  })
+  public name: string;
+
+  @ApiProperty({
+    description: 'The description of the product',
+    example: 'iPhone 15 description',
+  })
+  public description: string;
+
+  @ApiProperty({
+    description: 'The price of the product',
+    example: 1500000,
+  })
+  public price: number;
+
+  @ApiProperty({
+    description: 'The image URL of the product',
+    example: 'https://example.com/product1.jpg',
+  })
+  public imageUrl: string;
+}
