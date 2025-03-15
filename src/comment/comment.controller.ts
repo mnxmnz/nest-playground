7a936fa58f33c4dfbebbdae7ca829e6e1f6bf441
mnxmnz <minzidev@gmail.com>
2025-03-15 18:24:36 +0900

    feat: 댓글 CRUD 기능 추가 및 전체 댓글 조회 메서드 구현

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,17 +1,61 @@
-import { Controller, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  Get,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { Comment } from './entities/comment.entity';
+import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @Controller('comment')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
+
+  @Get('/all')
+  async getAllComments(): Promise<Comment[]> {
+    const comments = await this.commentService.getAllComments();
+    return comments;
+  }
 
   @Post('/:productId')
   async create(
     @Param('productId') productId: string,
     @Body() createCommentDto: CreateCommentDto,
   ): Promise<Comment> {
-    return await this.commentService.create(productId, createCommentDto);
+    return await this.commentService.createComment(productId, createCommentDto);
+  }
+
+  @Get('/:commentId')
+  async getCommentById(
+    @Param('commentId') commentId: string,
+  ): Promise<Comment> {
+    const comment = await this.commentService.getCommentById(commentId);
+    return comment;
+  }
+
+  @Put('/:commentId')
+  async updateCommentById(
+    @Param('commentId') commentId: string,
+    @Body() updateCommentDto: UpdateCommentDto,
+  ): Promise<Comment> {
+    const updatedComment = await this.commentService.updateCommentById(
+      commentId,
+      updateCommentDto,
+    );
+    return updatedComment;
+  }
+
+  @Delete('/:commentId')
+  async deleteCommentById(
+    @Param('commentId') commentId: string,
+  ): Promise<string> {
+    const deletedComment =
+      await this.commentService.deleteCommentById(commentId);
+    return deletedComment;
   }
 }
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Comment } from './entities/comment.entity';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { ProductService } from 'src/product/product.service';
+import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @Injectable()
 export class CommentService {
@@ -13,7 +14,12 @@ export class CommentService {
     private readonly productService: ProductService,
   ) {}
 
-  async create(
+  async getAllComments(): Promise<Comment[]> {
+    const comments = await this.commentRepository.find();
+    return comments;
+  }
+
+  async createComment(
     productId: string,
     createCommentDto: CreateCommentDto,
   ): Promise<Comment> {
@@ -25,5 +31,36 @@ export class CommentService {
     });
     await this.commentRepository.save(newComment);
     return newComment;
+  }
+
+  async getCommentById(commentId: string): Promise<Comment> {
+    const comment = await this.commentRepository.findOne({
+      where: { id: commentId },
+    });
+    if (!comment) {
+      throw new NotFoundException('Comment not found');
+    }
+    return comment;
+  }
+
+  async updateCommentById(
+    commentId: string,
+    updateCommentDto: UpdateCommentDto,
+  ): Promise<Comment> {
+    const updatedComment = await this.getCommentById(commentId);
+    if (!updatedComment) {
+      throw new NotFoundException('Comment not found');
+    }
+    await this.commentRepository.update(commentId, updateCommentDto);
+    return updatedComment;
+  }
+
+  async deleteCommentById(id: string): Promise<string> {
+    const deletedComment = await this.getCommentById(id);
+    if (!deletedComment) {
+      throw new NotFoundException('Comment not found');
+    }
+    await this.commentRepository.delete(deletedComment);
+    return 'Comment deleted successfully';
   }
 }
