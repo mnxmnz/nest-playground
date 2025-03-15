0598fbc0719a19f370838c5f05a20de978ee3099
mnxmnz <minzidev@gmail.com>
2025-03-15 14:44:11 +0900

    feat: 제품 모듈 추가 및 기본 CRUD 기능 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({}), DatabaseModule],
+  imports: [ConfigModule.forRoot({}), DatabaseModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
