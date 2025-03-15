07cc295477f5ccbbc937bd19c88a0d65ecfc26b2
mnxmnz <minzidev@gmail.com>
2025-03-15 15:53:21 +0900

    feat: 제품 엔티티 추가 및 TypeORM 설정 업데이트

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,19 @@
-export class Product {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  public id: string;
+
+  @Column()
+  public name: string;
+
+  @Column()
+  public description: string;
+
+  @Column()
+  public price: number;
+
+  @Column()
+  public imageUrl: string;
+}
