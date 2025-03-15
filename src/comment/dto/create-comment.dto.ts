a1ae6fab7175f39c7dec200f53af595e69c673c3
mnxmnz <minzidev@gmail.com>
2025-03-15 18:19:50 +0900

    feat: CreateCommentDto 및 CreateProductDto Swagger API 속성 추가

--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,3 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateCommentDto {
+  @ApiProperty({
+    description: 'The content of the comment',
+    example: 'This is a comment',
+  })
   public content: string;
 }
--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,6 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'The name of the product',
+    example: 'iPhone 15',
+  })
   name: string;
+
+  @ApiProperty({
+    description: 'The description of the product',
+    example: 'iPhone 15 description',
+  })
   description: string;
+
+  @ApiProperty({
+    description: 'The price of the product',
+    example: 1500000,
+  })
   price: number;
+
+  @ApiProperty({
+    description: 'The image URL of the product',
+    example: 'https://example.com/product1.jpg',
+  })
   imageUrl: string;
 }
